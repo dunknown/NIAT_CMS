496e52547b013d3ba7d25f16cfc74672497d86f4
Dmitriy Neizvestniy <d.unknown07@gmail.com>
2015-04-29 00:44:23 +0300

    Set up security

---
+++ src/main/java/com/niat/cms/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.niat.cms.security;
+
+import com.niat.cms.domain.User;
+import com.niat.cms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @author dunknown
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User '" + username + "' not found.");
+        }
+        return user;
+    }
+}
