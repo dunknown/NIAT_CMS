96a990b78f3160ca3b985f65e589a31f17a31379
Dmitriy Neizvestniy <d.unknown07@gmail.com>
2015-05-07 19:39:14 +0300

    Added @Reposity annotation to MaterialRepository

--- src/main/java/com/niat/cms/repo/MaterialRepository.java
+++ src/main/java/com/niat/cms/repo/MaterialRepository.java
@@ -3,6 +3,7 @@ package com.niat.cms.repo;
 import com.niat.cms.domain.Material;
 import com.niat.cms.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  *
  * @author dunknown
  */
+@Repository
 public interface MaterialRepository extends JpaRepository<Material, Long> {
     Material findById(long id);
     List<Material> findByOnMainTrue();
