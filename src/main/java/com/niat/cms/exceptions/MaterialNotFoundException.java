341c0196bcffb8239221aa9599537ced563edfd5
gtament <gtament@gmail.com>
2015-05-21 22:06:15 +0300

    setRole() now throws exception in case user tries to change own role. Added this custom exception. Added new role READER. Made it new user's default

---
+++ src/main/java/com/niat/cms/exceptions/MaterialNotFoundException.java
@@ -0,0 +1,11 @@
+package com.niat.cms.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author gtament
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such material")
+public class MaterialNotFoundException extends RuntimeException {
+}
