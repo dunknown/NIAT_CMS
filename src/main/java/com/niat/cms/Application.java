d8cae781d6ee5a9aea5470b93a178afd7ede654c
Dmitriy Neizvestniy <d.unknown07@gmail.com>
2015-06-16 23:13:16 +0300

    Replaced whitelabel error page with our custom

--- src/main/java/com/niat/cms/Application.java
+++ src/main/java/com/niat/cms/Application.java
@@ -1,13 +1,32 @@
 package com.niat.cms;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import java.util.EnumSet;
 
 /**
  * @author dunknown
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public FilterRegistrationBean getSpringSecurityFilterChainBindedToError(
+            @Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(springSecurityFilterChain);
+        registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        return registration;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
